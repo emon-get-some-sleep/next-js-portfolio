37bae5205ab615254b0ea2715333ec5c1fd92896
emonh0que <you@example.com>
2023-06-21 13:16:41 +0600

    added styles

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import styles from './page.module.css';
 const Page = () => {
     return (
-        <div>
-            Page
+        <div className={styles.container}>
+            Contact
         </div>
     );
 };
